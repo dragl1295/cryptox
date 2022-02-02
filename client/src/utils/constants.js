cf77ecf1fa7723c1c92efa6b29547a29207b4479
dragl1295 <dhemantgoyal@gmail.com>
2022-02-03 00:10:31 +0530

    crptox

---
+++ client/src/utils/constants.js
@@ -0,0 +1,4 @@
+import abi from './Transactions.json';
+
+export const contractABI = abi.abi;
+export const contractAddress = '0x35D8b78762B781Fe10Da33Ef5BfC204D2543CC1e';
---
+++ smart_contract/hardhat.config.js
@@ -0,0 +1,14 @@
+
+
+
+require('@nomiclabs/hardhat-waffle');
+
+module.exports = {
+  solidity: '0.8.0',
+  networks: {
+    ropsten: {
+      url: 'https://eth-ropsten.alchemyapi.io/v2/wNrdkXYRSy_IB4MfGAE6paeHlLN7hImu',
+      accounts: [ 'f6b3dfe93a728e47b59ed0884a6eb1ddda9dd592b755fca777515b949d062369' ]
+    }
+  }
+}
